fcb4354539b150e77b424b82d272f4babcc30470
vaillante <vaillantepaloma@gmail.com>
2022-08-26 01:01:10 -0700

    vava

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './UI/components/LoginComponents/login';
 // import Load from './UI/components/LoadingPage/load';
-import Home from './UI/pages/Home/home';
+// import Home from './UI/pages/Home/home';
 import Pagenotfound from './UI/components/PageNotFound/pagenotfound';
 function App() {
   // const [loading, setLoading] = useState(false);
@@ -17,12 +17,12 @@ function App() {
     // (loading ? <Load/> :(
     <div className="App">
      <Router>
-      <Login />
+      {/* <Login /> */}
        <Routes>
         
         
-         <Route path="/" element={<Home/>} />
-         <Route path="/login" element={<Login/>} />
+         {/* <Route path="/" element={<Home/>} /> */}
+         <Route path="/" element={<Login/>} />
          <Route path="/*" element={<Pagenotfound/>}/>
        </Routes>
      </Router>
